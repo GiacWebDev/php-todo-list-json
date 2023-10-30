dc9bfcf671b0c7ddd3b12bdd43875cee3e994c19
GiacWebDev <giacobechini@gmail.com>
2023-10-30 16:46:33 +0100

    [edit] index structure

--- index.php
+++ index.php
@@ -4,8 +4,27 @@
 
 <body>
 
-  <div id="app">
-  <h1>CIAO MONDO</h1>
+  <div id="app" class="container my-5 ">
+    <h1 class="text-center">TO DO LIST</h1>
+
+    <div class="container-fluid">
+
+      <ul class="list-group my-5">
+        <li class="list-group-item">An item</li>
+        <li class="list-group-item">A second item</li>
+        <li class="list-group-item">A third item</li>
+        <li class="list-group-item">A fourth item</li>
+        <li class="list-group-item">And a fifth one</li>
+      </ul>
+
+      <div class="input-group mb-3">
+        <input type="text" class="form-control" placeholder="Scrivi un nuovo task">
+        <button class="btn btn-primary" type="button" id="button-addon2">Button</button>
+      </div>
+
+
+    </div>
+
   </div>
   
   <!-- JS -->
--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // File per gestire l'interazione tra Frontend e VueJS
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      // harray task
+      tasks: [],
+      newTask: "",
+    };
+  },
+}).mount("#app");
