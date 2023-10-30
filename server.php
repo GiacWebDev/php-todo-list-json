464971818b55508f26148a7795e60ff8f90123d4
GiacWebDev <giacobechini@gmail.com>
2023-10-30 17:18:10 +0100

    [edit] array trasform string to array

--- server.php
+++ server.php
@@ -1,1 +1,11 @@
-<!-- File per gestire la logica del server -->
+<!-- File per gestire la logica del server leggendo il file json -->
+
+<?php
+
+// salvo il file JSON in una variabile sotto forma di stringa (non ciclabile ancora)
+$json_string = file_get_contents('task.json');
+var_dump($json_string);
+
+// la trasformo in un array php per essere utilizzata
+$list = json_decode($json_string);
+var_dump($list);
