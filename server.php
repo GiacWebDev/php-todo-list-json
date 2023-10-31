fd2241cb64bc1e919da5c23c50a73c394f0b659f
GiacWebDev <giacobechini@gmail.com>
2023-10-31 11:50:21 +0100

    [edit] add task to list in server

--- server.php
+++ server.php
@@ -10,8 +10,14 @@ $json_string = file_get_contents('task.json');
 // la trasformo in un array php per essere utilizzata
 $list = json_decode($json_string);
 
+//se e quando mi arriva in post il dato lo aggiungo alla lista e la salvo in .json
+if(isset($_POST['taskItem'])) {
+  $newTask = $_POST['taskItem'];
+  // pusho il nuovo elemento nella lista
+  $list[] = $newTask;
+  file_put_contents('task.json', json_encode($list));
+}
 
-
-// riporto i dati in .json 
+// riporto i dati in .json restituendo la lista aggiornata con nuove task
 header('Content-Type: application/json');
 echo json_encode($list);
